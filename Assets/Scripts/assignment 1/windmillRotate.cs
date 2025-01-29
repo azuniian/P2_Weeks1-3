d2d44deb82d28b97265cee24fc13b1c94f596b1d
PCZUNIIAN\azuni <azuniian895@gmail.com>
2025-01-28 23:11:34 -0500

    assignment 1 log 2: wrote rotation script for windmills, each with a public speed variable so the rotations/cycle can be varied attached rotation script to center of windmill sprite created & duplicated windmill out of sprite shapes (4 total windmills) ground shapes created

--- Assets/Scripts/assignment 1/windmillRotate.cs
+++ Assets/Scripts/assignment 1/windmillRotate.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class windmillRotate : MonoBehaviour
 {
+    public float rotationSpeed = 5f;
+    float rotationDirection = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class windmillRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rotationDirection *= -1;
+        }
+
+        transform.Rotate(0, 0, (rotationDirection * rotationSpeed) * Time.deltaTime);
     }
 }
