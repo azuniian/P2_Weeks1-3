094467afedb7a141763e4bee2a8e74cd5815ffa3
PCZUNIIAN\azuni <azuniian895@gmail.com>
2025-01-30 22:53:37 -0500

    assignment 1 log 4: created an animation curve and lerp script for the birb

---
+++ Assets/Scripts/assignment 1/birbMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birbMovement : MonoBehaviour
+{
+    
+    
+    [Range(0,1)]
+    public float speed = 0f;
+    public AnimationCurve animationCurve;
+    public Transform start;
+    public Transform end;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = start.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector2.Lerp(start.position, end.position, animationCurve.Evaluate(speed));
+
+        if (speed >= 1)
+        {
+            speed = 0;
+        }
+
+        if (speed < 1)
+        {
+            speed += (float)(0.1 * (Time.deltaTime));
+        }
+
+    }
+}
